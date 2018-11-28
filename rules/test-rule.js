42cf9422ec5e906eacfb7839b05dd37e6bbd38f3
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-11-29 00:46:20 +0900

    output page-query ast

--- rules/test-rule.js
+++ rules/test-rule.js
@@ -9,19 +9,20 @@ module.exports = {
     return {
       // Program ノード (AST のルート) で呼ばれる
       Program(node) {
-        console.log(node);
 
-        // if (!node.templateBody) {
-        //   return
-        // }
+        if (!node.templateBody) {
+          return
+        }
 
-        // // <template>の兄弟タグから <page-query> を探す
-        // const topLevelNodes = node.templateBody.parent.children
-        // for (const node of topLevelNodes) {
-        //   if (node.type === "VElement" && node.name === "page-query") {
-        //     // Do checking on the node.
-        //   }
-        // }
+
+
+        // <template>の兄弟タグから <page-query> を探す
+        const topLevelNodes = node.templateBody.parent.children
+        for (const node of topLevelNodes) {
+          if (node.type === "VElement" && node.name === "page-query") {
+            console.log(node);
+          }
+        }
       }
     }
   }
--- tests/fixture.vue
+++ tests/fixture.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>hoge</div>
+  <div>aaaa</div>
 </template>
+
+<script>
+const script = 111;
+</script>
+
+<page-query>
+query Blog {
+  allWordPressPost (limit: 5) {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+
+<style lang="scss" scoped>
+div {
+  font-size: 2rem;
+  padding: 10px;
+}
+</style>
